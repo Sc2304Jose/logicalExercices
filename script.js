8c33bf3556ea29e465774a6c52d9f44cb8b858fd
JOSE DIOMEDES DIOMEDES <71745956+Sc2304Jose@users.noreply.github.com>
2021-11-05 19:28:54 +0000

    arrow functions

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/**count numbers characters of chain text
+ * function declared and function expresed 
+ */
 function CountLetters(chain) {
     var count = 0;
     for (var i = 0; i <= chain.length; i++) {
@@ -10,18 +13,41 @@ function CountLetters(chain) {
 }
 //console.log(CountLetters("Hola Mundo"))
 
+//cont CountLetters2=(chain="") =>(!chain) ?console.warn("chain is empty") :console.info(chain.length))
+
+
+/**Fuctions cut Characther
+ * function declared and function expresed
+ */
 function CutCharacter(chain,numberCharacter){
 return chain.substring(0,numberCharacter);
-
 }
 
+//arow fuctions SplitChain.
+const  SplitChain = (chain = "",long = undefined) =>
+(!chain)
+?console.warn("chain is empty")
+:(long===undefined)
+  ?console.warn("long undefined")
+  :console.info(chain.substring(0,long))
+
+//console.log(SplitChain("hola mundo",4));
 //console.log(CutCharacter("Hola mundo",6));
 
-function SplitChain(chain){
+function SplitChain1(chain){
     return chain.split(" ");
 }
 
-//console.log(SplitChain("hola mundo"));
+const chaintoArray =(chain=" ", separator=" ")=>
+(!chain)
+?console.warn("chain is empty")
+:console.info(chain.split(separator))
+
+//chaintoArray("hello.fucking.monsters",".")
+
+// expres function
+
+
 
 
 function forchain(chain,number){
@@ -30,3 +56,15 @@ function forchain(chain,number){
     }   
 }
 //forchain("iuju",4);
+
+const forChain2 =(chain="", long=undefined)=>
+(!chain)
+?console.warn("chain is empty")
+:(long===undefined)
+  ?console.warn("long is undefined")
+  :console.log(chain*long)
+
+
+
+
+forChain2("iuju",4);
